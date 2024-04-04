112894581d59e3397880a149c9e268292c30b9f9
Marioporras24 <porrascmario@gmail.com>
2024-04-04 01:43:45 -0500

    wsgi

--- PuntoVenta/PuntoVenta/wsgi.py
+++ PuntoVenta/PuntoVenta/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import ventas
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PuntoVenta.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ventas.settings')
 
 application = get_wsgi_application()
